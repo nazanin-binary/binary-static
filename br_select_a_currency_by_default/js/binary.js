d0cb9173fa7bcd86c2c7465d6ee75b712c86a91b
nazaninreihani <nazanin.reyh@gmail.com>
2019-01-09 09:41:22 -0500

    Deploy to select_a_currency_by_default

--- br_select_a_currency_by_default/js/binary.js
+++ br_select_a_currency_by_default/js/binary.js
@@ -32465,11 +32465,11 @@ var SetCurrency = function () {
 
                 var localized_text = '';
                 if (isCryptocurrency($(this).attr('id'))) {
-                    localized_text = localize('You have chosen [_1] as the currency for this account. You cannot change this later. You can have more than one cryptocurrency account.', $(this).attr('id'));
+                    localized_text = localize('You have chosen <strong>[_1]</strong> as the currency for this account. You cannot change this later. You can have more than one cryptocurrency account.', $(this).attr('id'));
                 } else {
-                    localized_text = localize('You have chosen [_1] as the currency for this account. You cannot change this later. You can have one fiat currency account only.', $(this).attr('id'));
+                    localized_text = localize('You have chosen <strong>[_1]</strong> as the currency for this account. You cannot change this later. You can have one fiat currency account only.', $(this).attr('id'));
                 }
-                $popup_content.text(localized_text).setVisibility(1);
+                $popup_content.html(localized_text).setVisibility(1);
                 $('body').append($('<div/>', { id: 'set_currency_popup_container', class: 'lightbox' }).append($popup_container.clone().setVisibility(1)));
 
                 var $popup = $(popup_selector);

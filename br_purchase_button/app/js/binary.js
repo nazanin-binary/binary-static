66728a6772d094f9eb393f2e60bb79dff68e6004
nazaninreihani <nazanin.reyh@gmail.com>
2019-01-21 21:37:24 -0500

    Deploy to purchase_button

--- br_purchase_button/app/js/binary.js
+++ br_purchase_button/app/js/binary.js
@@ -19994,18 +19994,20 @@ var getProposalInfo = exports.getProposalInfo = function getProposalInfo(store, 
     var stake = proposal.display_value;
     var basis_list = store.basis_list;
 
-    var contract_basis = basis_list ? basis_list.find(function (o) {
+    var contract_basis = basis_list.find(function (o) {
         return o.value !== store.basis;
-    }) : {};
-    var has_increased = proposal[contract_basis.value] > obj_prev_contract_basis.value;
+    });
+    var is_stake = contract_basis.text === 'Stake';
+    var price = is_stake ? stake : proposal[contract_basis.value];
+    var has_increased = price > obj_prev_contract_basis.value;
 
-    if (proposal[contract_basis.value] === obj_prev_contract_basis.value) {
+    if (price === obj_prev_contract_basis.value) {
         has_increased = null;
     }
 
     var obj_contract_basis = {
         text: contract_basis.text || '',
-        value: (contract_basis.text === 'Stake' ? stake : proposal[contract_basis.value]) || ''
+        value: price || ''
     };
 
     return {

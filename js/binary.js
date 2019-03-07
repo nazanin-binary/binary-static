31f4cb56e8059f81813e36fc50a350a5c53eb039
nazanin <reihani>
2019-03-07 14:23:07 -0500

    Deploy to gh-pages

--- js/binary.js
+++ js/binary.js
@@ -17792,7 +17792,6 @@ var AssetIndexUI = function () {
 
     var onLoad = function onLoad() {
         $container = $('#asset-index');
-        $('#empty-asset-index').setVisibility(0);
         asset_index = market_columns = undefined;
         active_symbols = undefined;
 
@@ -17808,8 +17807,6 @@ var AssetIndexUI = function () {
 
     var populateTable = function populateTable() {
         if (!active_symbols || !asset_index) return;
-
-        if (!asset_index.length) $('#empty-asset-index').setVisibility(1);
 
         $('#errorMsg').setVisibility(0);
         asset_index = AssetIndex.getAssetIndexData(asset_index, active_symbols);

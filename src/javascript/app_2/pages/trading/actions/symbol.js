3d9a10c1d0252f15bb40ed535e43e6ada4def3a8
nazaninreihani <nazanin.reyh@gmail.com>
2019-05-10 11:59:11 -0400

    Deploy to gh-pages

--- src/javascript/app_2/pages/trading/actions/symbol.js
+++
@@ -1,8 +0,0 @@
-import ContractType from '../store/logic/contract_type';
-
-export const onSymbolChangeAsync = function *({symbol}) {
-    const r = yield ContractType.getContractsList(symbol);
-    return {
-        contract_types_list: r,
-    };
-};

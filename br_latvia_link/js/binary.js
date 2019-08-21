225e71f2a6133ae2e17f128d8838292f7caebd91
nazanin-binary <nazanin.reyh@gmail.com>
2019-08-21 06:42:04 -0400

    Deploy to latvia_link

--- br_latvia_link/js/binary.js
+++ br_latvia_link/js/binary.js
@@ -9782,7 +9782,7 @@ var BinaryLoader = function () {
                 });
             }
         } else if (config.not_authenticated && Client.isLoggedIn()) {
-            if (this_page === 'home') {
+            if (this_page === 'home' || this_page === 'new-account') {
                 BinaryPjax.load('' + Client.defaultRedirectUrl() + window.location.search, true);
             } else {
                 handleNotAuthenticated();
@@ -34622,7 +34622,7 @@ var binary_desktop_app_id = 14473;
 
 var getAppId = function getAppId() {
     var app_id = null;
-    var user_app_id = 12447; // you can insert Application ID of your registered application here
+    var user_app_id = ''; // you can insert Application ID of your registered application here
     var config_app_id = window.localStorage.getItem('config.app_id');
     var is_new_app = /\/app\//.test(window.location.pathname);
     if (config_app_id) {
